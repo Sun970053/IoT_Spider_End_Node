913459f4df331b3e55b7d9e25b1b42ea5b3701fa
don-gua-dadada <93250006+don-gua-dadada@users.noreply.github.com>
2023-10-21 22:36:27 +0800

    Android Settings Activity added SharedPreference
    Successfully using SharePreference to save the switchOn switch bar state.
    The switch bar state will be hold even user leave the Settings Activity page.

--- MK200_Android_ver1/app/src/main/java/com/example/mk200_android_ver1/SettingsActivity.java
+++ MK200_Android_ver1/app/src/main/java/com/example/mk200_android_ver1/SettingsActivity.java
@@ -3,18 +3,24 @@ package com.example.mk200_android_ver1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.android.material.materialswitch.MaterialSwitch;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private SwitchMaterial switchOn, switchLight, switchMode;
+    private MaterialSwitch switchOn, switchLight, switchMode;
     private RelativeLayout templateRobotRoute, templateRobotManualMode;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,45 @@ public class SettingsActivity extends AppCompatActivity {
 
         templateRobotRoute = findViewById(R.id.templateRobotRoute);
         templateRobotManualMode = findViewById(R.id.templateRobotManualMode);
+
+        sharedPreferences = getSharedPreferences("switchOn", MODE_PRIVATE);
+        boolean isCheckedOn = sharedPreferences.getBoolean("switchOn", true);
+        switchOn.setChecked(isCheckedOn);
+
+        switchOn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(SettingsActivity.this, "Robot Turn On", Toast.LENGTH_SHORT).show();
+                    sharedPreferences.edit().putBoolean("switchOn", true).apply();
+                } else {
+                    Toast.makeText(SettingsActivity.this, "Robot Turn Off", Toast.LENGTH_SHORT).show();
+                    sharedPreferences.edit().putBoolean("switchOn", false).apply();
+                }
+            }
+        });
+
+        switchLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(SettingsActivity.this, "Light Turn On", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SettingsActivity.this, "Light Turn Off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(SettingsActivity.this, "Power Saving Mode", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SettingsActivity.this, "Normal Mode", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         templateRobotRoute.setOnClickListener(new View.OnClickListener() {
             @Override
