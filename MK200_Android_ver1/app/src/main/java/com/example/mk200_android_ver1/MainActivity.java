aa08e82b8f13e56f3ed69a67383042ac0a79fd24
don-gua-dadada <93250006+don-gua-dadada@users.noreply.github.com>
2023-10-21 20:10:11 +0800

    Android Settings Activity update
    connected Settings Activity to Main Activity with button

--- MK200_Android_ver1/app/src/main/java/com/example/mk200_android_ver1/MainActivity.java
+++ MK200_Android_ver1/app/src/main/java/com/example/mk200_android_ver1/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.mk200_android_ver1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStatus, btnSettings, btnNews, btnDataReport, btnMap, btnShutDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStatus = findViewById(R.id.btnStatus);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnNews = findViewById(R.id.btnNews);
+        btnDataReport = findViewById(R.id.btnDataReport);
+        btnMap = findViewById(R.id.btnMap);
+        btnShutDown =findViewById(R.id.btnShutDown);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
