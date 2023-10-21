06fbf4b866836cdb1994208a36e6707c5fef6688
don-gua-dadada <93250006+don-gua-dadada@users.noreply.github.com>
2023-10-21 16:24:22 +0800

    Android add activity
    add setting activity

---
+++ MK200_Android_ver1/app/src/main/java/com/example/mk200_android_ver1/StatusActivity.java
@@ -0,0 +1,14 @@
+package com.example.mk200_android_ver1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
